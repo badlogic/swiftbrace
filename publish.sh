7a408444a730ccba9a2d7fe1ddffeb8cf6c72c02
Mario Zechner <badlogicgames@gmail.com>
2024-08-09 20:24:31 +0200

    Initial commit

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+blargh --in src --out html
+host=slayer.marioslab.io
+host_dir=/home/badlogic/swiftbrace.
+current_date=$(date "+%Y-%m-%d %H:%M:%S")
+commit_hash=$(git rev-parse HEAD)
+echo "{\"date\": \"$current_date\", \"commit\": \"$commit_hash\"}" > html/version.json
+
+rsync -avz --exclude node_modules --exclude .git --exclude data --exclude docker/data ./ $host:$host_dir
+
+if [ "$1" == "server" ]; then
+    echo "Publishing client & server"
+    ssh -t $host "cd $host_dir && ./docker/control.sh stop && ./docker/control.sh start && ./docker/control.sh logs"
+else
+    echo "Publishing client only"
+fi
---
+++ stats.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+host=slayer.marioslab.io
+host_dir=/home/badlogic/mariozechner.at
+scp $host:$host_dir/docker/data/logs/access.log access.log
+goaccess --keep-last=30 -f access.log -o report.html --log-format=COMBINED
+rm access.log
+open report.html
